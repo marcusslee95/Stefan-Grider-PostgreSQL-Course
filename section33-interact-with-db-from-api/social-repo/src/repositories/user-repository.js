e9494b4a15e8bc4afc01209db79c61d223a627a9
Marcus Lee <marcusslee95@berkeley.edu>
2021-02-26 22:31:32 -0800

    created UsersRepository which is the one way to interact with Users table + implemented the get /users endpoint

---
+++ section33-interact-with-db-from-api/social-repo/src/repositories/user-repository.js
@@ -0,0 +1,31 @@
+const pool = require('../pool')
+
+class UsersRepository {
+    static async findAll(){//purpose of async is to say this function could take a long time to execute so don't wait for this code to finish to do other stuff. Just keep doing your stuff and when we get back result we'll send it over to you
+        const result = await pool.query('SELECT * FROM users') // purpose of this await is to say before running code below wait for this to finish
+
+        return result.rows
+    }
+
+    static async findById(){
+
+    }
+
+    static async findById(){
+
+    }
+
+    static async findById(){
+
+    }
+
+    static async findById(){
+
+    }
+
+    static async findById(){
+
+    }
+}
+
+module.exports = UsersRepository
--- section33-interact-with-db-from-api/social-repo/src/routes/users.js
+++ section33-interact-with-db-from-api/social-repo/src/routes/users.js
@@ -2,7 +2,15 @@ const express = require('express')
 
 const router = express.Router()
 
-router.get('/users', async (req, res) => {})
+const UsersRepository = require('../repositories/user-repository')
+
+router.get('/users', async (req, res) => {
+    //run a query to get all users
+    const users = await UsersRepository.findAll()
+    
+    //Send result back to person who made the request
+    res.send(users)
+})
 
 router.get('/users/:id', async (req, res) => {})
 
